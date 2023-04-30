939e33c52d3989c63c2b51f903d4b913070ba804
Alberto <albertomrcs97@gmail.com>
2023-04-30 17:57:52 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Search music!</h1>
+      <header>
+        <form className='form'>
+          <input placeholder='Eminem, Julio Iglesias...' />
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+      <main>
+        content
+      </main>
+    </div>
+  )
+}
+
+export default App
