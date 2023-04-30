30e20e7c67f8c9185d5c1e45b2c354b0dd1b7fbe
Alberto <albertomrcs97@gmail.com>
2023-04-30 18:12:51 +0200

    List songs using mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import './App.css'
+import './App.css';
+
+import results from './mocks/results.json';
 
 function App() {
+  const songs = results.data;
+  const hasSongs = results.total > 0;
+
   return (
     <div className='app'>
       <h1>Search music!</h1>
@@ -11,7 +16,25 @@ function App() {
         </form>
       </header>
       <main>
-        content
+        {
+          hasSongs
+            ? (
+              <ul>
+              {
+                songs.map((song) => (
+                  <li key={song.id}>
+                    <h3>{song.title}</h3>
+                    <img src={song.album.cover_medium} alt={song.album.title} />
+                    <audio src={song.preview} />
+                  </li>
+                ))
+              } 
+              </ul>
+            )
+          : (
+            <p>There is no songs for that query</p>
+          )
+        }
       </main>
     </div>
   )
