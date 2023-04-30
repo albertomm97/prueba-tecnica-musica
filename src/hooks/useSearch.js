9494f6225358f1c7c2c73cbce2b5b0fbbf004b37
Alberto <albertomrcs97@gmail.com>
2023-04-30 19:21:25 +0200

    Use useRef to check if input has been use for the first time

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function useSearch() {
   const [query, setQuery] = useState("");
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
-    setQuery(query);
+    if (isFirstInput.current) {
+      isFirstInput.current = query === "";
+      return;
+    }
 
     if (query === "") {
       setError("Empty query");
